72afa4bb15765002fb4187eea446d24d177efeda
nampole <tumon@isaiah-pc>
2017-03-12 19:49:42 +0900

    4장 추가

---
+++ first.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<h1>Hello Web!</h1>";
+    echo "<h1>안녕 웹!</h1>";
+?>
---
+++ helloworld.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>A PHP script including HTML</title>
+</head>
+<body>
+<h1><?php echo "hello world"; ?></h1>
+<h3><?php echo "안녕 세계"; ?></h3>
+</body>
+</html>
