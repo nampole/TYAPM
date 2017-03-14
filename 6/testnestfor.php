067a86de67fd628be26f117321446a6e7a56331d
tumon <tumon@isaiah-pc>
2017-03-14 21:35:15 +0900

    수정

--- 6/testnestfor.php
+++ 6/testnestfor.php
@@ -1,8 +1,8 @@
 <?php
 echo "<table style=\"border: 1px solid #000;\"> \n";
-for ($y=1; $y<=12; $y++) {
+for ($y=1; $y<=120; $y++) {
 	echo "<tr> \n";
-	for ($x=1; $x<=12; $x++) {
+	for ($x=1; $x<=120; $x++) {
 		echo "<td style=\"border: 1px solid #000; width: 25px; text-align:center;\">";
 		echo ($x * $y);
 		echo "</td> \n";
